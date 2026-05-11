dab125717ebed02dc4a6eb91b58052efc791f952
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-11 13:51:34 +0300

    feat: add the input field

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export interface Todo {
   name: string;
@@ -12,9 +12,24 @@ export default function Page() {
     { name: "Finish this app.", isDone: false },
     { name: "Do something else.", isDone: true },
   ]);
+  const [input, setInput] = useState<string>("");
+
+  const handleNewTodo = (e: FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="min-h-screen bg-sky-50 p-3 flex gap-3 flex-col">
+      <form onSubmit={(e) => handleNewTodo(e)}>
+        <input
+          type="text"
+          placeholder="Enter your task..."
+          className="bg-white p-3 w-full rounded-md"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      <p>{input}</p>
       {todos.map((todo, index) => (
         <div
           key={index}
