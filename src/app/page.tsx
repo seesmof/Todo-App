3f1015c4ffe036f4a2de4b1e86c2a8ff6f75fd5f
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-21 10:58:13 +0300

    feat: list all todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
 "use client";
 
-import New from "@/components/New";
-import { SubmitEventHandler } from "react";
+import { useState } from "react";
 
-const handleSubmit = (e: SubmitEventHandler<HTMLFormElement>) => {
-  e.preventDefault();
-};
+export interface Todo {
+  id: number;
+  name: string;
+  isDone: boolean;
+}
 
 export default function Page() {
-  return <New handleSubmit={handleSubmit} />;
+  const [items, setItems] = useState<Todo[]>([
+    { id: 0, name: "Finish this app", isDone: false },
+    { id: 1, name: "Learn React", isDone: true },
+    { id: 2, name: "Learn Next.js", isDone: false },
+  ]);
+
+  return (
+    <>
+      <form className="flex flex-row gap-3 p-3">
+        <input className="outline flex-1 px-1" type="text" />
+        <button className="outline px-3">Add</button>
+      </form>
+      <hr />
+      <div className="flex flex-col gap-3 p-3">
+        {items.map((todo) => (
+          <div key={todo.id} className="flex flex-row outline p-3 gap-3">
+            <button
+              className={`outline px-3 ${todo.isDone ? "bg-green-200" : "bg-sky-200"}`}
+            >
+              {todo.isDone ? "Done" : "To Do"}
+            </button>
+            <p>{todo.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
